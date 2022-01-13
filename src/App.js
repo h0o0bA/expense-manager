856e91b7b54c1f36a77f3d101de1b3a89ed3058e
Ihab Ali <47971028+h0o0bA@users.noreply.github.com>
2022-01-13 17:49:36 -0600

    total expenses

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { AppProvider } from "./context/AppContext";
+import ExpenseTotal from "./components/ExpenseTotal";
 import AddExpenseForm from "./components/AddExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 
@@ -10,6 +11,11 @@ const App = () => {
     <AppProvider>
       <div className="container">
         <h1>Expense Manager</h1>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <ExpenseTotal />
+          </div>
+        </div>
         <br />
         <h3 className="mt-3">User Expenses</h3>
         <div className="row ">
